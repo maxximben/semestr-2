b2ba1e691a3b204b57763b31c571457f3c339d35
maxximben <maximben888@gmail.com>
2025-02-28 08:40:24 +0600

    Update Скобки(недоделанный).cs

--- Скобки(недоделанный).cs
+++ Скобки(недоделанный).cs
@@ -5,89 +5,95 @@ class Program
 {
     static void Main()
     {
-
         Console.WriteLine("Введи строку:");
         string str = Console.ReadLine();
 
         char[] charArray = str.ToCharArray();
 
-        Stack<char> stack1 = new Stack<char>();
-        Stack<char> stack2 = new Stack<char>();
-        Stack<char> stack3 = new Stack<char>();
-
+        Stack<char> stack = new Stack<char>();
 
         for (int i = 0; i < charArray.Length; i++)
         {
-
             if (charArray[i] == '(')
             {
-                stack1.Push(charArray[i]);
+                stack.Push(charArray[i]);
             }
             else if (charArray[i] == '{')
             {
-                stack2.Push(charArray[i]);
+                stack.Push(charArray[i]);
             }
             else if (charArray[i] == '[')
             {
-                stack3.Push(charArray[i]);
+                stack.Push(charArray[i]);
             }
-            else if (charArray[i] == ')' && stack1.Count > 0)
+            else if (charArray[i] == ')')
             {
-                
-                if (stack1.Count == 0)
+                if (stack.Count == 0)
                 {
                     Console.WriteLine("Скобки расставлены неправильно");
                     Environment.Exit(0);
                 }
-                stack1.Pop();
-            }
-            else if (charArray[i] == '}' && stack2.Count > 0)
-            {
-                if (stack2.Count == 0)
+                if (stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else
                 {
                     Console.WriteLine("Скобки расставлены неправильно");
                     Environment.Exit(0);
                 }
-                stack2.Pop();
             }
-            else if (charArray[i] == ']' && stack3.Count > 0)
+            else if (charArray[i] == ']')
             {
-                if (stack3.Count == 0) {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("Скобки расставлены неправильно");
+                    Environment.Exit(0);
+
+                }
+                if (stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else
+                {
                     Console.WriteLine("Скобки расставлены неправильно");
                     Environment.Exit(0);
                 }
-                stack3.Pop();
             }
-
+            else if (charArray[i] == '}')
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("Скобки расставлены неправильно");
+                    Environment.Exit(0);
+                }
+                if (stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    Console.WriteLine("Скобки расставлены неправильно");
+                    Environment.Exit(0);
+                }
+            }
         }
 
-        Console.WriteLine("Элементы стека1 :");
-        foreach (var item in stack1)
-        {
-            Console.WriteLine(item);
-        }
-
-        Console.WriteLine("Элементы стека2 :");
-        foreach (var item in stack2)
-        {
-            Console.WriteLine(item);
-        }
-
-        Console.WriteLine("Элементы стека3 :");
-        foreach (var item in stack3)
-        {
-            Console.WriteLine(item);
-        }
-
-        if (stack1.Count > 0 || stack2.Count > 0 || stack3.Count > 0)
-        {
-            Console.WriteLine("Скобки расставлены неправильно");
-        }
-        else
+        if (stack.Count == 0)
         {
             Console.WriteLine("Скобки расставлены правильно");
         }
 
+        else
+        {
+            Console.WriteLine("Скобки расставлены неправильно");
+        }
 
+        /*Console.WriteLine("Элементы стека :");
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }*/
     }
 }
